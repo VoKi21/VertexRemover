f742d344ca94dd8f980f9049284d67cbbdfcf3d8
VoKi21 <vovakilchenko@gmail.com>
2023-12-29 10:43:54 +0300

    Optimized the code. it seems to work

--- src/com/cgvsu/Main.java
+++ src/com/cgvsu/Main.java
@@ -2,6 +2,8 @@ package com.cgvsu;
 
 import com.cgvsu.model.Model;
 import com.cgvsu.objreader.ObjReader;
+import com.cgvsu.objwriter.ObjWriter;
+import com.cgvsu.vertexremover.VertexRemover;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,7 +13,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Path fileName = Path.of("3DModels/Faceform/WrapHead.obj");
+        //Path fileName = Path.of("3DModels/Faceform/WrapHead.obj");
+        Path fileName = Path.of("3DModels/SimpleModelsForReaderTests/Teapot.obj");
         String fileContent = Files.readString(fileName);
 
         System.out.println("Loading model ...");
@@ -21,5 +24,14 @@ public class Main {
         System.out.println("Texture vertices: " + model.textureVertices.size());
         System.out.println("Normals: " + model.normals.size());
         System.out.println("Polygons: " + model.polygons.size());
+
+        VertexRemover.removeVertex(model, 435);
+
+        System.out.println("Vertices: " + model.vertices.size());
+        System.out.println("Texture vertices: " + model.textureVertices.size());
+        System.out.println("Normals: " + model.normals.size());
+        System.out.println("Polygons: " + model.polygons.size());
+
+        ObjWriter.write(model, "outputs/out.obj");
     }
 }
--- src/com/cgvsu/vertexremover/VertexRemover.java
+++ src/com/cgvsu/vertexremover/VertexRemover.java
@@ -4,6 +4,7 @@ import com.cgvsu.model.Model;
 import com.cgvsu.model.Polygon;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class VertexRemover {
     public static void removeVertex(Model model, int index) {
@@ -21,84 +22,49 @@ public class VertexRemover {
     }
 
     private static void reindex(Model model) {
-        Set<Integer> unusedVertices = new HashSet<>();
-        Set<Integer> unusedTextures = new HashSet<>();
-        Set<Integer> unusedNormals = new HashSet<>();
-        for (int i = 0; i < model.vertices.size(); i++) {
-            unusedVertices.add(i);
-        }
-        for (int i = 0; i < model.textureVertices.size(); i++) {
-            unusedTextures.add(i);
-        }
-        for (int i = 0; i < model.normals.size(); i++) {
-            unusedNormals.add(i);
-        }
+        Set<Integer> unusedVertices = IntStream
+                .range(0, model.vertices.size()).boxed().collect(HashSet::new, Set::add, Set::addAll);
+        Set<Integer> unusedTextures = IntStream
+                .range(0, model.textureVertices.size()).boxed().collect(HashSet::new, Set::add, Set::addAll);
+        Set<Integer> unusedNormals = IntStream
+                .range(0, model.normals.size()).boxed().collect(HashSet::new, Set::add, Set::addAll);
+
         for (Polygon polygon : model.polygons) {
-            for (int vertexIndex : polygon.getVertexIndices()) {
-                unusedVertices.remove(vertexIndex);
-            }
-            for (int textureIndex : polygon.getTextureVertexIndices()) {
-                unusedTextures.remove(textureIndex);
-            }
-            for (int normalIndex : polygon.getNormalIndices()) {
-                unusedNormals.remove(normalIndex);
-            }
+            polygon.getVertexIndices().forEach(unusedVertices::remove);
+            polygon.getTextureVertexIndices().forEach(unusedTextures::remove);
+            polygon.getNormalIndices().forEach(unusedNormals::remove);
         }
 
-        Stack<Integer> verticesToRemove = new Stack<>();
-        Stack<Integer> texturesToRemove = new Stack<>();
-        Stack<Integer> normalsToRemove = new Stack<>();
-
-        verticesToRemove.addAll(unusedVertices.stream().sorted().toList());
-        texturesToRemove.addAll(unusedTextures.stream().sorted().toList());
-        normalsToRemove.addAll(unusedNormals.stream().sorted().toList());
-
-        while (!verticesToRemove.isEmpty()) {
-            int index = verticesToRemove.pop();
-            model.vertices.remove(index);
-            for (Polygon polygon : model.polygons){
-                for (int i = 0; i < polygon.getVertexIndices().size(); i++) {
-                    if (polygon.getVertexIndices().get(i) > index) {
-                        polygon.getVertexIndices().set(i, polygon.getVertexIndices().get(i) - 1);
-                    }
-                }
-            }
-        }
-
-        while (!texturesToRemove.isEmpty()) {
-            int index = texturesToRemove.pop();
-            model.textureVertices.remove(index);
-            for (Polygon polygon : model.polygons){
-                for (int i = 0; i < polygon.getTextureVertexIndices().size(); i++) {
-                    if (polygon.getTextureVertexIndices().get(i) > index) {
-                        polygon.getTextureVertexIndices().set(i, polygon.getTextureVertexIndices().get(i) - 1);
-                    }
-                }
-            }
-        }
-
-        while (!normalsToRemove.isEmpty()) {
-            int index = normalsToRemove.pop();
-            model.normals.remove(index);
-            for (Polygon polygon : model.polygons){
-                for (int i = 0; i < polygon.getNormalIndices().size(); i++) {
-                    if (polygon.getNormalIndices().get(i) > index) {
-                        polygon.getNormalIndices().set(i, polygon.getNormalIndices().get(i) - 1);
-                    }
-                }
-            }
-        }
+        removeAndReindex(model, unusedVertices, model.vertices);
+        removeAndReindex(model, unusedTextures, model.textureVertices);
+        removeAndReindex(model, unusedNormals, model.normals);
     }
 
-    public static void main(String[] args) {
-        List<Integer> l = new ArrayList<>();
-        l.add(0);
-        l.add(1);
-        l.add(2);
-        Stack<Integer> s = new Stack<>();
-        s.addAll(l);
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
+    private static <T> void removeAndReindex(Model model, Set<Integer> unusedIndices, List<T> originalVertices) {
+        Stack<Integer> indicesToRemove = new Stack<>();
+        indicesToRemove.addAll(unusedIndices.stream().sorted().toList());
+
+        while (!indicesToRemove.isEmpty()) {
+            int index = indicesToRemove.pop();
+            System.out.println(originalVertices.get(index));
+            originalVertices.remove(index);
+
+            for (Polygon polygon : model.polygons) {
+                ArrayList<Integer> polygonIndices = new ArrayList<>();
+                if (originalVertices == model.vertices) {
+                    polygonIndices = polygon.getVertexIndices();
+                } else if (originalVertices == model.textureVertices) {
+                    polygonIndices = polygon.getTextureVertexIndices();
+                } else if (originalVertices == model.normals) {
+                    polygonIndices = polygon.getNormalIndices();
+                }
+
+                for (int i = 0; i < polygonIndices.size(); i++) {
+                    if (polygonIndices.get(i) > index) {
+                        polygonIndices.set(i, polygonIndices.get(i) - 1);
+                    }
+                }
+            }
         }
     }
 }
