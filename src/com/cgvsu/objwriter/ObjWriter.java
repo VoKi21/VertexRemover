4852db0a42ae2f7f0b62fdc2f11f0d854f25426f
VoKi21 <vovakilchenko@gmail.com>
2023-12-29 13:05:04 +0300

    Changed ArrayList to List where possible, applied IDE suggestions

--- src/com/cgvsu/objwriter/ObjWriter.java
+++ src/com/cgvsu/objwriter/ObjWriter.java
@@ -12,45 +12,46 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ObjWriter {
     private static final String OBJ_VERTEX_TOKEN = "v";
     private static final String OBJ_TEXTURE_TOKEN = "vt";
     private static final String OBJ_NORMAL_TOKEN = "vn";
     private static final String OBJ_FACE_TOKEN = "f";
-    private static final String OBJ_GROUP_TOKEN = "g";
 
     public static void write(Model model, String fileName) throws IOException {
         File file = getObjFileByPath(fileName);
         //если файла с таким именем нет, то создаем новый файл с именем newFile.obj в этом проекте
         //если же закинули директорию то создаем новый файл с именем newFile.obj в этой директории
-        PrintStream fileWriter = new PrintStream(file);
-        fileWriter.println(getContent(model));
+        try (PrintStream fileWriter = new PrintStream(file)) {
+            fileWriter.println(getContent(model));
+        }
 
     }
     public static String getContent(Model model){
         String modelContent = "";
-        ArrayList<Vector3f> vertices = model.vertices;
-        ArrayList<Vector3f> normals = model.normals;
-        ArrayList<Polygon> polygons = model.polygons;
-        ArrayList<Vector2f> textureVertices = model.textureVertices;
+        List<Vector3f> vertices = model.vertices;
+        List<Vector3f> normals = model.normals;
+        List<Polygon> polygons = model.polygons;
+        List<Vector2f> textureVertices = model.textureVertices;
 
         checkVerticesExistence(vertices);
 
         modelContent+=getStringVerticesContent(vertices)+"\n";
         StringBuilder normalsInfo = getStringNormalsContent(normals);
-        if (!normalsInfo.toString().equals("")){
+        if (!normalsInfo.toString().isEmpty()){
             modelContent+=normalsInfo+"\n";
         }
         StringBuilder textureVerticesInfo = getStringTextureVerticesContent(textureVertices);
-        if (!textureVerticesInfo.toString().equals("")){
+        if (!textureVerticesInfo.toString().isEmpty()){
             modelContent+=textureVerticesInfo+"\n";
         }
         modelContent+=getStringPolygonsContent(polygons)+"\n";
         return modelContent;
     }
 
-    protected static void checkVerticesExistence(ArrayList<Vector3f> vertices) {
+    protected static void checkVerticesExistence(List<Vector3f> vertices) {
         if (vertices == null) {
             throw new NullPointerException("List of vertices is null");
         }
@@ -59,49 +60,49 @@ public class ObjWriter {
         }
     }
 
-    protected static StringBuilder getStringVerticesContent(ArrayList<Vector3f> vertices) {
+    protected static StringBuilder getStringVerticesContent(List<Vector3f> vertices) {
         StringBuilder str = new StringBuilder();
         for (Vector3f v : vertices) {
-            str.append(OBJ_VERTEX_TOKEN + " " + v.coordstoStringSplitBySpace() + "\n");
+            str.append(OBJ_VERTEX_TOKEN + " ").append(v.coordstoStringSplitBySpace()).append("\n");
         }
-        str.append("# " + vertices.size() + " vertices\n");
+        str.append("# ").append(vertices.size()).append(" vertices\n");
         return str;
 
     }
 
-    protected static StringBuilder getStringTextureVerticesContent(ArrayList<Vector2f> textureVertices) {
+    protected static StringBuilder getStringTextureVerticesContent(List<Vector2f> textureVertices) {
         StringBuilder str = new StringBuilder();
         if (textureVertices == null || textureVertices.isEmpty()) {
             return str;
         }
         for (Vector2f vt : textureVertices) {
-            str.append(OBJ_TEXTURE_TOKEN + " " + vt.coordstoStringSplitBySpace() +  "\n");
+            str.append(OBJ_TEXTURE_TOKEN + " ").append(vt.coordstoStringSplitBySpace()).append("\n");
         }
-        str.append("# " + textureVertices.size() + " texture coords\n");
+        str.append("# ").append(textureVertices.size()).append(" texture coords\n");
         return str;
     }
 
-    protected static StringBuilder getStringNormalsContent(ArrayList<Vector3f> normals) {
+    protected static StringBuilder getStringNormalsContent(List<Vector3f> normals) {
         StringBuilder str = new StringBuilder();
         if (normals == null || normals.isEmpty()) {
             return str;
         }
         for (Vector3f vn : normals) {
-            str.append(OBJ_NORMAL_TOKEN + " " + vn.coordstoStringSplitBySpace() + "\n");
+            str.append(OBJ_NORMAL_TOKEN + " ").append(vn.coordstoStringSplitBySpace()).append("\n");
         }
-        str.append("# " + normals.size() + " vertex normals\n");
+        str.append("# ").append(normals.size()).append(" vertex normals\n");
         return str;
     }
 
-    protected static StringBuilder getStringPolygonsContent(ArrayList<Polygon> polygons) {
+    protected static StringBuilder getStringPolygonsContent(List<Polygon> polygons) {
         StringBuilder str = new StringBuilder();
         if (polygons == null || polygons.isEmpty()) {
             return str;
         }
         for (Polygon polygon : polygons) {
-            str.append(OBJ_FACE_TOKEN + " " + polygonInfoInObjForm(polygon) + "\n");
+            str.append(OBJ_FACE_TOKEN + " ").append(polygonInfoInObjForm(polygon)).append("\n");
         }
-        str.append("# " + polygons.size() + " polygons\n");
+        str.append("# ").append(polygons.size()).append(" polygons\n");
         return str;
     }
 
@@ -131,17 +132,17 @@ public class ObjWriter {
         if (!textureVertexIndices.isEmpty()) {
             isTextured = true;
             for (int i = 0; i < oneVertexInPolygonInfoString.size(); i++) {
-                oneVertexInPolygonInfoString.get(i).append(slash + (textureVertexIndices.get(i) + 1));
+                oneVertexInPolygonInfoString.get(i).append(slash).append(textureVertexIndices.get(i) + 1);
             }
         }
         if (!normalIndices.isEmpty()) {
             slash = (isTextured) ? "/" : "//";
             for (int i = 0; i < oneVertexInPolygonInfoString.size(); i++) {
-                oneVertexInPolygonInfoString.get(i).append(slash + (normalIndices.get(i) + 1));
+                oneVertexInPolygonInfoString.get(i).append(slash).append(normalIndices.get(i) + 1);
             }
         }
         for (StringBuilder info : oneVertexInPolygonInfoString) {
-            str.append(info + " ");
+            str.append(info).append(" ");
         }
         return str;
     }
@@ -155,7 +156,7 @@ public class ObjWriter {
             return file;
         }
         if (!Files.exists(path)) {
-            filePath = (filePath.equals("")) ? "newFile.obj" : getFileName(filePath);
+            filePath = (filePath.isEmpty()) ? "newFile.obj" : getFileName(filePath);
             file = new File(filePath);
             file.createNewFile();
             return file;
